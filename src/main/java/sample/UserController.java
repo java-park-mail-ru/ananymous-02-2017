5f07326cea81fd3691ec50b326dbe28446a06088
Solovyev Konstantin <k.solovyev@my.com>
2017-02-14 01:27:10 +0300

    add comments

--- src/main/java/sample/UserController.java
+++ src/main/java/sample/UserController.java
@@ -11,8 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @RequestMapping(path = "/api/user/{userId}", method = RequestMethod.GET)
+
+
+    /**
+     * Данный метод вызывается с помощью reflection'a, поэтому Spring позволяет инжектить в него аргументы.
+     * Подробнее можно почитать в сорцах к аннотации {@link RequestMapping}. Там описано как заинжектить различные атрибуты http-запроса.
+     * Возвращаемое значение можно так же варьировать. Н.п. Если отдать InputStream, можно стримить музыку или видео
+     */
+    @RequestMapping(path = "/api/user/{userId}", method = RequestMethod.GET, produces = "application/json")
     public String getMsg(@PathVariable(name = "userId") int userId) {
         return "{\"userId\":" + userId + '}';
+    }
+
+    /**
+     * Конструктор тоже будет вызван с помощью reflection'а. Другими словами, объект создается через ApplicationContext.
+     * Поэтому в нем можно использовать DI. Подробнее про это расскажу на лекции.
+     */
+    public UserController() {
     }
 }
