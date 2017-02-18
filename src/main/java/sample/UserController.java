01b2a987cd5f0273d3e148a998c732b956ac68ac
Solovyev Konstantin <k.solovyev@my.com>
2017-02-18 15:34:14 +0300

    lesson changes

--- src/main/java/sample/UserController.java
+++ src/main/java/sample/UserController.java
@@ -1,9 +1,10 @@
 package sample;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Solovyev on 09/02/2017.
@@ -18,9 +19,10 @@ public class UserController {
      * Подробнее можно почитать в сорцах к аннотации {@link RequestMapping}. Там описано как заинжектить различные атрибуты http-запроса.
      * Возвращаемое значение можно так же варьировать. Н.п. Если отдать InputStream, можно стримить музыку или видео
      */
-    @RequestMapping(path = "/api/user/{userId}", method = RequestMethod.GET, produces = "application/json")
-    public String getMsg(@PathVariable(name = "userId") int userId) {
-        return "{\"userId\":" + userId + '}';
+    @RequestMapping(path = "/api/user", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public GetMsgRequest getMsg(@RequestBody GetMsgRequest body, HttpSession httpSession) {
+        httpSession.setAttribute("userId", body.getUserId());
+        return body;
     }
 
     /**
@@ -28,5 +30,18 @@ public class UserController {
      * Поэтому в нем можно использовать DI. Подробнее про это расскажу на лекции.
      */
     public UserController() {
+    }
+
+    private static final class GetMsgRequest {
+        int userId;
+
+        @JsonCreator
+        public GetMsgRequest(@JsonProperty("userId") int userId) {
+            this.userId = userId;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
     }
 }
