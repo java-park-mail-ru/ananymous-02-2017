414450bdbb24100a6e2e037f9d037b8f6b967caa
Denis <choccocro@choccocro.local>
2017-03-10 18:20:55 +0300

    delete AppConfig

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,11 +1,19 @@
 package application;
 
+import application.db.Database;
+import application.db.DatabaseFakeImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Database database() {
+        return new DatabaseFakeImpl();
     }
 }
