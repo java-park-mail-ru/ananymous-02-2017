1c1550129f08cf8bfa1afeaf2508745942f22778
Chocco-Crokko <zastiralkin@gmail.com>
2017-06-01 00:28:23 +0300

    test for old name

--- src/main/java/application/websocket/Message.java
+++ src/main/java/application/websocket/Message.java
@@ -12,7 +12,7 @@ public class Message {
     @NotNull
     public static final String SNAPSHOT = "Snapshot";
     @NotNull
-    public static final String REMOVE_PLAYER = "Remove";
+    public static final String REMOVE_PLAYER = "RemovePlayer";
 
     @Nullable
     private String type;
