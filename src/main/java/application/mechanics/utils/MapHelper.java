753007a833ad237ccb4c75adaffb3ff7fd6768dc
Chocco-Crokko <zastiralkin@gmail.com>
2017-06-01 00:24:40 +0300

    add missed 2

--- src/main/java/application/mechanics/utils/MapHelper.java
+++ src/main/java/application/mechanics/utils/MapHelper.java
@@ -4,6 +4,6 @@ import application.mechanics.Config;
 import application.mechanics.base.Map;
 
 public class MapHelper {
-    public static final double X_OFFSET = Map.M * Config.BLOCK_SIZE + Config.BLOCK_SIZE / 2;
-    public static final double Z_OFFSET = Map.N * Config.BLOCK_SIZE + Config.BLOCK_SIZE / 2;
+    public static final double X_OFFSET = (Map.M / 2) * Config.BLOCK_SIZE + Config.BLOCK_SIZE / 2;
+    public static final double Z_OFFSET = (Map.N / 2) * Config.BLOCK_SIZE + Config.BLOCK_SIZE / 2;
 }
